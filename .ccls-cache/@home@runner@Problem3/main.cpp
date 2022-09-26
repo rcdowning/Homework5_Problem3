5711a10a5abea00d5d0d7518316a64e92f0cb2b5
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-09-26 06:28:14 +0000

    Table Output

--- .ccls-cache/@home@runner@Problem3/main.cpp
+++ .ccls-cache/@home@runner@Problem3/main.cpp
@@ -13,17 +13,20 @@ int main()
   double I; // wind speed index
 
   // Out loop to run first WCI formula, will convert to degrees F and mi/hr
-    cout << "Wind Chill Index" << endl;
+    cout << "WCI" << setw(25) << "Temp (degrees F)" << setw(25) << "Wind Speed (mi/hr)" << endl;
+
     
     for (int Ta = 2; Ta <= 10; Ta = Ta+4)
       {
         // calculation for index, converting to F and mi/hr
-          I = 35.74+0.6125*Ta-35.75*pow(Ws, 0.16)+0.4275*Ta*pow(Ws, 0.16);
-          cout << I << endl;
+           I = 35.74+0.6125*Ta-35.75*pow(Ws, 0.16)+0.4275*Ta*pow(Ws, 0.16);
+           cout << I << endl;
+           cout << setw(20) << Ta << endl;
+        
 
         for (Ws=5; Ws <= 11; Ws = Ws+2)
           {
-
+            cout << setw(45) << Ws << endl;
             
           }
 
